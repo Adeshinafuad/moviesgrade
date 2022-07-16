bb52849c2e6cd3b72c01c940b8804199018edc07
adeshinafuad <adeshinafuad@gmail.com>
2022-07-16 22:03:15 +0100

    changing allowed host

--- moviesgrade/settings.py
+++ moviesgrade/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = config("DJANGO_SECRET_KEY")
-print(SECRET_KEY, "\n")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
-print(DEBUG, "\n")
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
